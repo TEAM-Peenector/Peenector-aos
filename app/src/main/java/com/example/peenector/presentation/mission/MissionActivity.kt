2a1c6d28705995e299d93a2b75881e1232dd1682
sub101 <subin10@nate.com>
2022-10-07 16:55:16 +0900

    미션뷰 생성

---
+++ app/src/main/java/com/example/peenector/presentation/mission/MissionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.peenector.presentation.mission
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.peenector.R
+
+class MissionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mission)
+    }
+}
