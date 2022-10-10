726b49f958a6c714e95a8c88bc8999513b833cf0
yoon <cbj0010@naver.com>
2022-10-10 15:34:07 +0900

    feature/1 signup signin 구현 회원가입에서 학번이랑 pw 입력시 signin으로 넘어오게 끔 구현, 로그인 버튼 클릭시 main으로 intent

--- app/src/main/java/com/example/peenector/presentation/login/SigninActivity.kt
+++ app/src/main/java/com/example/peenector/presentation/login/SigninActivity.kt
@@ -1,15 +1,63 @@
 package com.example.peenector.presentation.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.example.peenector.MainActivity
 import com.example.peenector.databinding.ActivitySigninBinding
 
 class SigninActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySigninBinding
+    private var classOf: String? = null
+    private var password: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySigninBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        onClickSignUp()
+        clickLogin()
     }
+
+    private fun onClickSignUp() {
+        binding.tvGotoSignup.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startForResult.launch(intent)
+        }
+    }
+
+     val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            resultLaunchSignUp(result)
+        }
+
+    private fun resultLaunchSignUp(result: ActivityResult) {
+        if (result.resultCode == RESULT_OK) {
+            result.data?.let {
+                //result.data가 null이면 let함수가 실행되지 않음
+                classOf = it.getStringExtra("classOf")
+                password = it.getStringExtra("passWord")
+                binding.signinEdtClassof.setText(classOf)
+                binding.signinEdtPassword.setText(password)
+            }
+        }
+    }
+
+
+    private fun clickLogin() {
+        binding.signinBtnLogin.setOnClickListener {
+            if (binding.signinEdtClassof.text.toString() == classOf && binding.signinEdtPassword.text.toString() == password) {
+                Toast.makeText(this, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "회원가입이랑 정보가 다릅니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
