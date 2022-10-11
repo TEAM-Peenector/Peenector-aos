b37b824be265d109b1c80791be0adfc258c89e2b
sub101 <subin10@nate.com>
2022-10-11 20:34:23 +0900

    메인뷰 미션페이지 클릭리스너 & 미션뷰 마이페이지클릭리스너 구현

--- app/src/main/java/com/example/peenector/MainActivity.kt
+++ app/src/main/java/com/example/peenector/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import com.example.peenector.databinding.ActivityMainBinding
 import com.example.peenector.presentation.main.MainModel
 import com.example.peenector.presentation.main.MainRecyclerAdapter
+import com.example.peenector.presentation.mission.MissionActivity
 import com.example.peenector.presentation.mypage.MypageActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -53,6 +54,11 @@ class MainActivity : AppCompatActivity() {
         //마이페이지버튼 클릭이벤트리스너
         btn_main_mypage.setOnClickListener{
             var intent = Intent(this, MypageActivity::class.java)
+            startActivity(intent)
+        }
+        //미션페이지버튼 클릭이벤트리스너
+        btn_main_missionpage.setOnClickListener{
+            var intent = Intent(this,MissionActivity::class.java)
             startActivity(intent)
         }
     }
